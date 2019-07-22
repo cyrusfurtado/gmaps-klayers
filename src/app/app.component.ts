1814c7dff52177bbb6d31a93214d062c2fa11973
cyrus furtado <cyrus_furtado@persistent.co.in>
2019-07-22 16:44:37 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('map', null) mapElement: ElementRef;
+  map: google.maps.Map;
+
   title = 'GMapsKLayers';
+
+  ngOnInit() {
+    const mapProperties = {
+      center: new google.maps.LatLng(35.2271, -80.8431),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+  }
+
 }
